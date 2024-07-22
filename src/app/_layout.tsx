a32d73f7a225cab0fc2eb249e4054fd154d657b8
JUtupe <jutuper@gmail.com>
2024-07-22 16:31:54 +0200

    chore: biome

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,14 @@
 import { AuthContextProvider } from "@/context/AuthContext";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import { Slot, useGlobalSearchParams, usePathname } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { useFonts } from "expo-font";
 import { useEffect } from "react";
-import {
-	DarkTheme,
-	DefaultTheme,
-	ThemeProvider,
-} from "@react-navigation/native";
 import "@/global.css";
 import "react-native-reanimated";
 import analytics from "@react-native-firebase/analytics";
@@ -17,35 +17,35 @@ import analytics from "@react-native-firebase/analytics";
 SplashScreen.preventAutoHideAsync();
 
 export default function Root() {
-	const pathname = usePathname();
-	const params = useGlobalSearchParams();
+  const pathname = usePathname();
+  const params = useGlobalSearchParams();
 
-	const colorScheme = useColorScheme();
-	const [loaded] = useFonts({
-		SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
-	});
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
+  });
 
-	useEffect(() => {
-		if (loaded) {
-			SplashScreen.hideAsync();
-		}
-	}, [loaded]);
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
-	useEffect(() => {
-		analytics().logScreenView({
-			screen_name: pathname,
-		});
-	}, [pathname, params]);
+  useEffect(() => {
+    analytics().logScreenView({
+      screen_name: pathname,
+    });
+  }, [pathname]);
 
-	if (!loaded) {
-		return null;
-	}
+  if (!loaded) {
+    return null;
+  }
 
-	return (
-		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-			<AuthContextProvider>
-				<Slot />
-			</AuthContextProvider>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <AuthContextProvider>
+        <Slot />
+      </AuthContextProvider>
+    </ThemeProvider>
+  );
 }
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,16 +1,16 @@
-import { createClient } from "@supabase/supabase-js";
+import type { Database } from "@/types/supabase-db-types";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Database } from "@/types/supabase-db-types";
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient<Database>(
-	process.env.EXPO_PUBLIC_SUPABASE_URL!,
-	process.env.EXPO_PUBLIC_SUPABASE_KEY!,
-	{
-		auth: {
-			storage: AsyncStorage,
-			autoRefreshToken: true,
-			persistSession: true,
-			detectSessionInUrl: false,
-		},
-	},
+  process.env.EXPO_PUBLIC_SUPABASE_URL!,
+  process.env.EXPO_PUBLIC_SUPABASE_KEY!,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  },
 );
